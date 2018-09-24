548464327ea9de148ef0b3de39184ef41d7a564c
Tomas Pessanha <td.pessanha@gmail.com>
2018-09-24 20:56:08 +0100

    Worked on fileTransfer

--- src/scripts/fileTransfer.ts
+++ src/scripts/fileTransfer.ts
@@ -1,69 +1,129 @@
+import fs from "fs";
 import net from "net";
 
+interface IConnectionTransfer {
+	fileSize: number;
+	checksum: number;
+	writer: fs.WriteStream | null;
+}
+
+interface ISocketFileHeaderMessage {
+	fileSize: number;
+	fileName: string;
+	checksum: number;
+}
+/**
+ * File server class is the server part of the file transfer implementation,
+ * To use create a new FileServer and run listening
+ */
 export default class FileServer {
 	//Vars
-	private _running: boolean;
-	private server: net.Server | null;
+	private server: net.Server;
 
 	public constructor(
 		private readonly HOST: string,
 		private readonly PORT: number
 	) {
-		this._running = false;
-		this.server = null;
+		this.server = this.createServer();
 	}
 
-	get running(): boolean {
-		return this._running;
+	get listening(): boolean {
+		return this.server.listening;
 	}
 
-	public start() {
-		if (!this.server && !this.running) {
-			this._running = true;
-			this.server = this.createServer();
+	/**
+	 * Start listening for connections
+	 */
+	public listen() {
+		if (!this.server.listening) {
 			this.server.listen(this.PORT, this.HOST);
 		} else {
 			throw new Error(`Server already running`);
 		}
 	}
 
-	public stop() {
+	/**
+	 * Prevents new connections, existing connections will remain active.
+	 */
+	public close() {
 		if (this.server) {
-			this._running = false;
 			this.server.close();
-			this.server = null;
 		}
 	}
 
+	private JsonParse(str: string): any {
+		try {
+			return JSON.parse(str);
+		} catch (e) {
+			return null;
+		}
+	}
+
+	//connection.writer = fs.createWriteStream("", { fd });
 	private createServer(): net.Server {
 		const server = net.createServer(socket => {
-			socket.setEncoding("utf8");
-
-			socket.on("data", data => {
+			const stats: IConnectionTransfer = {
+				fileSize: 0,
+				checksum: 0,
+				writer: null
+			};
+			socket.on("data", buffer => {
 				// tslint:disable-next-line:no-console
-				console.log(`data received: ${data}`);
+				console.log("Reading");
+
+				try {
+					const json = this.JsonParse(
+						buffer.toString()
+					) as ISocketFileHeaderMessage;
+					if (json != null) {
+						stats.fileSize = json.fileSize;
+						stats.checksum = json.checksum;
+
+						const fd = fs.openSync(json.fileName, "ax");
+						stats.writer = fs.createWriteStream("", { fd });
+						socket.pipe(stats.writer);
+						socket.write(new Buffer([0]));
+					}
+				} catch (error) {
+					socket.destroy();
+					if (error.code === "EEXIST") {
+						throw new Error("File already exists");
+					} else {
+						throw error;
+					}
+				}
 			});
+
+			socket.on("end", () => {
+				if (
+					stats.writer != null &&
+					stats.fileSize === stats.writer.bytesWritten &&
+					stats.fileSize > 0
+				) {
+					// tslint:disable-next-line:no-console
+					console.log("Received file");
+				} else {
+					// tslint:disable-next-line:no-console
+					console.log("File corrupted");
+				}
+				if (stats.writer !== null) {
+					stats.writer.end();
+					socket.end();
+				}
+				stats.writer = null;
+				stats.checksum = 0;
+				stats.fileSize = 0;
+
+				// tslint:disable-next-line:no-console
+				console.log("ended");
+			});
+		});
+
+		server.on("close", () => {
+			// tslint:disable-next-line:no-console
+			console.log("Server closed");
 		});
 
 		return server;
 	}
-
-	// server = net.createServer(socket => {
-	// 	// tslint:disable-next-line:no-console
-	// 	console.log(`CONNECTED: ${socket.remoteAddress}:${socket.remotePort}`);
-
-	// 	socket.setEncoding("utf8");
-
-	// 	socket.on("data", data => {
-	// 		// tslint:disable-next-line:no-console
-	// 		console.log("data received");
-	// 		// tslint:disable-next-line:no-console
-	// 		console.log("data is: " + data);
-	// 	});
-	// });
-
-	// const HOST = "127.0.0.1";
-	// //const FILEPATH = "/home/steve/Downloads/";
-
-	// server.listen(PORT, HOST);
 }
