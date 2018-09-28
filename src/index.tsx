564a0fd8bcc15245db0016361e187ad007ad4f90
Tomas Pessanha <td.pessanha@gmail.com>
2018-09-28 01:21:03 +0100

    Added service worker registration for offline caching

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { render } from "react-dom";
 import { AppContainer } from "react-hot-loader";
 import { BrowserRouter } from "react-router-dom";
 import App from "./components/App";
+import registerServiceWorker from "./scripts/registerServiceWorker";
 
 render(
 	<AppContainer>
@@ -12,3 +13,4 @@ render(
 	</AppContainer>,
 	document.getElementById("root")
 );
+registerServiceWorker();
