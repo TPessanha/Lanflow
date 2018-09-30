317cf44306eddbb40f180f228647c0ed3de43a27
Tomas Pessanha <td.pessanha@gmail.com>
2018-09-30 21:03:29 +0100

    Updated babel-loader to version 8 Use string templates

--- src/scripts/registerServiceWorker.ts
+++ src/scripts/registerServiceWorker.ts
@@ -27,7 +27,7 @@ export default function register() {
 			window.location.toString()
 		);
 
-		console.log("PUBLIC URL IS: " + publicUrl);
+		console.log(`PUBLIC URL IS: ${publicUrl}`);
 		if (publicUrl.origin !== window.location.origin) {
 			// Our service worker won't work if PUBLIC_URL is on a different origin
 			// from what our page is served on. This might happen if a CDN is used to
