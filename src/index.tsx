3e3c809ac12e741fe7d59914df6ac0ebfd902e7e
Tomas Pessanha <td.pessanha@gmail.com>
2018-10-07 13:00:25 +0100

    Removed: Service worker

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import { render } from "react-dom";
 import { AppContainer } from "react-hot-loader";
 import { BrowserRouter } from "react-router-dom";
 import App from "./components/App";
-import registerServiceWorker from "./scripts/registerServiceWorker";
 
 render(
 	<AppContainer>
@@ -13,4 +12,3 @@ render(
 	</AppContainer>,
 	document.getElementById("root")
 );
-registerServiceWorker();
